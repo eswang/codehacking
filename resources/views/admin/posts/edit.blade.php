4d4b03c544fbb1d493dc7334ba1f3fe5da8a9d60
Edward <edward@self.org>
2017-01-18 16:54:39 -0800

    able to create post with static category

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,1 +1,5 @@
-<?php
+@extends('layouts.admin')
+
+@section('content')
+	<h1>Edit Post</h1>
+@stop
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<h1 class="text-center">Oops no page avilable</h1>
+
+@stop
